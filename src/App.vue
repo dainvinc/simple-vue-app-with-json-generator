27e461da829b4bff12528b8037cbbf172558336f
Vishal <vishalinvincible21@gmail.com>
2021-12-23 15:00:15 -0500

    Pilot commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <img alt="Vue logo" src="./assets/logo.png">
+      <div class="p-1r" v-if="!profiles.length">
+        Here's some random data generated using json-generator.com
+        Click on the below button to get started...
+      </div>
+    </div>
+    <button @click="getRandomData" v-if="!profiles.length">Load profiles</button>
+    <div v-if="isLoading">Loading...</div>
+    <div v-for="profile in profiles" :key="profile.id">
+      <Card :email="profile.email" :personalDetails="profile.profile" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      isLoading: false,
+      profiles: [],
+    };
+  },
   components: {
-    HelloWorld
+    Card
+  },
+  methods: {
+    getRandomData() {
+      this.isLoading = true;
+
+      axios
+        .get('https://api.json-generator.com/templates/tfe-NATUyD71/data?access_token=1jimxiblxnop07n9r0red3toqgguhrke60ldjyfe')
+        .then(({ data }) => {
+          console.log(data);
+          this.profiles = data;
+          this.isLoading = false;
+        });
+    }
   }
 }
 </script>
@@ -24,5 +54,26 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.p-5 {
+  padding: 5px;
+}
+
+.p-1r {
+  padding: 1rem;
+}
+
+button {
+  border: 0;
+  border-radius: 5px;
+  padding: 12px;
+  background-color: lightgreen;
+  font-weight: 600;
+}
+
+button:hover {
+  background-color: rgb(85, 255, 0);
+  cursor: pointer;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    <h1>{{ personalDetails.name }}</h1>
+    <div class="space-between">
+      <div>
+        {{ personalDetails.dob }} 
+      </div>
+      <div>
+        {{ email }}
+      </div>
+    </div>
+    <div class="p-5">{{ personalDetails.about }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    email: String,
+    personalDetails: Object,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.hello {
+  width: 80%;
+  box-shadow: 5px 5px 5px lightgray;
+  padding: 5px;
+  margin: 1rem auto;
+}
+
+.space-between {
+  display: flex;
+  justify-content: space-between;
+  color: gray;
+  padding: 1rem;
+}
+</style>
